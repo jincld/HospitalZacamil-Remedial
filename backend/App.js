f41ed70c9cccdb0dd82bffab498c8d501cf224a3
Your Name <you@example.com>
2025-04-29 08:11:29 -0600

    Subir cambios recuperacion

--- backend/App.js
+++ backend/App.js
@@ -1,6 +1,9 @@
 //importar todo de la librería de express
 import express from "express";
 import cookieParser from "cookie-parser";
+import registerDoctorsRoute from './src/routes/registerDoctors.js';
+import doctorsRoute from './src/routes/doctors.js';
+import passwordRecoveryRoute from './src/routes/passwordRecovery.js';
 //crear constante que es igual a la libreria que importé y se ejecuta
 const app = express();
 
@@ -10,7 +13,9 @@ app.use (express.json());
 app.use(cookieParser());
 
 //ruta para crud
-
+app.use("/api/doctors", doctorsRoute);
+app.use("/api/registerDoctors", registerDoctorsRoute);
+app.use("/api/passwordRecovery", passwordRecoveryRoute);
 
 //importo esta constante para usar express en todos lados
 export default app;
